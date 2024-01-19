b18452be06b117e1dc4cefb69942a389d539fb04
Giovanni vignone <giovannivignone@Giovannis-MacBook-Pro.local>
2024-01-18 16:31:28 -0800

    consolidating logic into helpers

---
+++ src/utils/helpers.js
@@ -0,0 +1,12 @@
+export const getFunctionNameFromNode = (node) => {
+    switch (node.expression.type) {
+      case 'MemberAccess':
+        return node.expression.memberName;
+      case 'Identifier':
+        return node.expression.name;
+      case 'TypeNameExpression':
+        return node.expression.typeName;
+      default:
+        throw new Error(`Unsupported function call type: ${node.expression.type}`);
+    }
+  }
