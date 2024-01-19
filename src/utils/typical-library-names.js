6d89f7be57fa2142c3319c1b29258c6c38e4d3bc
Giovanni vignone <giovannivignone@Giovannis-MacBook-Pro.local>
2024-01-18 16:31:14 -0800

    default paths

---
+++ src/utils/typical-library-names.js
@@ -0,0 +1,16 @@
+// All typical library names in solidity
+
+export const typicalLibraryNames = [
+    'openzeppelin-contracts',
+    'openzeppelin-solidity',
+    'openzeppelin',
+    'solady',
+    'forge-std',
+    'BitMath',
+    'SafeMath',
+    'FullMath',
+    'FixedPoint',
+    'node_modules',
+    '@openzeppelin',
+]
+    
